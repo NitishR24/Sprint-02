4b39924c36d039735ba709d8e55c02cf75dce01c
NitishR24 <nitish.18cs096@cambridge.edu.in>
2022-05-22 22:35:30 +0530

    Add project files.

---
+++ MVCAppToBlob/Models/Attendance.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAppToBlob.Models
+{
+    /// <summary>
+    /// This class contains defination of attendence
+    /// </summary>
+    public class Attendence
+    {
+        /// <summary>
+        /// Roll number of the Student
+        /// </summary>
+        [DisplayName("ROLL NUMBER")]
+        public string RollNo { get; set; }
+
+        /// <summary>
+        /// First Name of the Student
+        /// </summary>
+        [DisplayName("FIRST NAME")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last Name of the Student
+        /// </summary>
+        [DisplayName("LAST NAME")]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Email Id of the Student
+        /// </summary>
+        [DisplayName("EMAIL ID")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Math marks
+        /// </summary>
+        [DisplayName("MATH")]
+        public int Math { get; set; }
+
+        /// <summary>
+        /// Physics marks
+        /// </summary>
+        [DisplayName("PHYSICS")]
+        public int Physics { get; set; }
+
+        /// <summary>
+        /// Chemistry marks
+        /// </summary>
+        [DisplayName("CHEMISTRY")]
+        public int Chemistry { get; set; }
+
+        /// <summary>
+        /// Average marks of all the subjects.
+        /// </summary>
+        [DisplayName("AVERAGE MARKS")]
+        public float Average { get => (Math + Physics + Chemistry) / 3; }
+    }
+
+}
